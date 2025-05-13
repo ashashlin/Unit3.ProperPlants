2097f5a9e383b28951d5838361cd9161a0992399
Ashley Lin <linashleyy@gmail.com>
2025-05-14 00:55:44 +0800

    Separate CartItems into smaller components

---
+++ src/components/Cart/CartItem.jsx
@@ -0,0 +1,17 @@
+export default function CartItem({ item }) {
+  return (
+    // is subject to change
+    <div className="cart-item">
+      <div className="cart-item-details">
+        {item.image}
+        {item.name}
+      </div>
+
+      <div className="cart-item-quantity-container">
+        <button className="subtract-quantity">-</button>
+        <span className="cart-item-quantity">{item.quantity}</span>
+        <button className="add-quantity">+</button>
+      </div>
+    </div>
+  );
+}
--- src/components/Cart/CartItems.jsx
+++ src/components/Cart/CartItems.jsx
@@ -1,18 +1,10 @@
+import CartItem from "./CartItem";
+
 export default function CartItems({ cart }) {
   return (
-    // is subject to change
     <section className="cart-items">
       {cart.map((item) => (
-        <div key={item.id} className="cart-item">
-          <span className="cart-item-img">{item.image}</span>
-          <span className="cart-item-name">{item.name}</span>
-
-          <div className="cart-item-quantity-container">
-            <button className="subtract-quantity">-</button>
-            <span className="cart-item-quantity">{item.quantity}</span>
-            <button className="add-quantity">+</button>
-          </div>
-        </div>
+        <CartItem key={item.id} item={item} />
       ))}
     </section>
   );
